24e1fa883eb945cfcaecfccd31255616fe218b35
wenzhong888 <143521544+wenzhong888@users.noreply.github.com>
2023-10-31 13:11:10 +0800

    Create dz.js

---
+++ dz.js
@@ -0,0 +1,47 @@
+var rule = {
+    title:'',//规则标题,没有实际作用,但是可以作为cms类名称依据
+    编码:'',//不填就默认utf-8
+    host:'',//网页的域名根,包含http头如 https://www,baidu.com
+    homeUrl:'/latest/',//网站的首页链接,可以是完整路径或者相对路径,用于分类获取和推荐获取 fyclass是分类标签 fypage是页数
+    url:'/fyclass/fypage.html[/fyclass/]',//网站的分类页面链接
+    detailUrl:'https://yanetflix.com/voddetail/fyid.html',//非必填,二级详情拼接链接,感觉没啥卵用
+    searchUrl:'',//搜索链接 可以是完整路径或者相对路径,用于分类获取和推荐获取 **代表搜索词 fypage代表页数
+    searchable:0,//是否启用全局搜索,
+    quickSearch:0,//是否启用快速搜索,
+    filterable:0,//是否启用筛选,
+    filter:{},// 筛选条件字典
+    // 筛选网站传参,会自动传到分类链接下(本示例中的url参数)-url里参数为fyfilter,可参考蓝莓影视.js
+    filter_url:'style={{fl.style}}&zone={{fl.zone}}&year={{fl.year}}&fee={{fl.fee}}&order={{fl.order}}',
+    // 注意,由于猫有配置缓存,搜索配置没法热加载，修改了js不需要重启服务器
+    // 但是需要tv_box进设置里换源使配置重新装载
+    headers:{//网站的请求头,完整支持所有的,常带ua和cookies
+        'User-Agent':'MOBILE_UA',
+        "Cookie": "searchneed=ok"
+    },
+    timeout:5000,//网站的全局请求超时,默认是3000毫秒
+    class_name:'电影&电视剧&动漫&综艺',//静态分类名称拼接
+    class_url:'1&2&3&4',//静态分类标识拼接
+    //动态分类获取 列表;标题;链接;正则提取 不需要正则的时候后面别加分号
+    class_parse:'#side-menu:lt(1) li;a&&Text;a&&href;com/(.*?)/',
+    // 除开全局过滤之外还需要过滤哪些标题不视为分类
+    cate_exclude:'',
+    // 除开全局动态线路名过滤之外还需要过滤哪些线路名标题不视为线路
+    tab_exclude:'',
+    // 服务器解析播放
+    play_parse:true,
+    // 自定义免嗅
+    lazy:'',
+    // 首页推荐显示数量
+    limit:6,
+    double:true,//是否双层列表定位,默认false
+    // 类似海阔一级 列表;标题;图片;描述;链接;详情 其中最后一个参数选填
+    // 如果是双层定位的话,推荐的第2段分号代码也是第2层定位列表代码
+    推荐:'.col-sm-6;h3&&Text;img&&data-src;.date&&Text;a&&href',
+    // 类似海阔一级 列表;标题;图片;描述;链接;详情 其中最后一个参数选填
+    一级:'.col-sm-6;h3&&Text;img&&data-src;.date&&Text;a&&href',
+    // 二级可以是*,表示规则无二级,直接拿一级的链接进行嗅探
+    // 或者 {title:'',img:'',desc:'',content:'',tabs:'',lists:''} 同海阔dr二级
+    二级:'*',
+    // 搜索可以是*,集成一级，或者跟一级一样的写法 列表;标题;图片;描述;链接;详情
+    搜索:'*',
+}
